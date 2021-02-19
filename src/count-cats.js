98832d49a5035075db47c3f32eb067bb91d01a8c
Iaroslav Silkin <maxfri1995@gmail.com>
2021-02-19 14:00:59 +0300

    refactor: edit count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,13 +3,11 @@ module.exports = function countCats(backyard) {
     const cat = '^^';
     let count = 0;
 
-    backyard.reduce(function (previtem, item) {
-        return item.reduce(function (prevelement, element) {
-            if (element === cat) {
-                count++;
-            }
-            return count;
-        }, 0)
+    backyard.flat().reduce(function (previtem, item) {
+        if (item === cat) {
+            count++;
+        }
+        return count;
     }, 0)
 
     return count;
