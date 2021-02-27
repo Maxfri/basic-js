329323f3afc81433f7a7e3d9bee3c24645e86721
maxfri <maxfri1995@gmail.com>
2021-02-28 00:29:32 +0300

    feat: add the real solution for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,18 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== "string" || isNaN( String.fromCharCode(sampleActivity.keyCode))) {
+  if (
+    typeof sampleActivity !== "string" ||
+    !Number(sampleActivity) ||
+    sampleActivity < 0 ||
+    sampleActivity > 15
+  ) {
     return false;
   }
+  let k = Math.LN2 / HALF_LIFE_PERIOD;
+  let ACTIVITY = Math.log(MODERN_ACTIVITY / sampleActivity);
+  let time = Math.ceil(ACTIVITY / k);
+
+  return time;
 };
